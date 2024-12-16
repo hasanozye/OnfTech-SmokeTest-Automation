27af65bf477f4ed8ed88342b96025365df85816c
hasan.ozyer <hasanozyer06@gmail.com>
2024-12-17 00:05:26 +0300

    Added Hooks class with screenshot functionality for test scenarios 📸

---
+++ src/test/java/stepdefs/Hooks.java
@@ -0,0 +1,36 @@
+package stepdefs;
+
+import driver.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import readers.property.PropertyReader;
+
+public class Hooks {
+
+    /*@After
+    public void tearDown() {
+        if (Driver.getDriver() != null) {
+            Driver.quitDriver();
+        }
+    }*/
+
+    @AfterStep
+    public void afterStepSS(Scenario scenario) {
+        boolean scrShot = PropertyReader.read().getProperty("takescreenshot").equalsIgnoreCase("true");
+        String takeScrOn = PropertyReader.read().getProperty("takescreenshot.on");
+        if (scrShot) {
+            if (takeScrOn.equalsIgnoreCase("fail")) {
+                if (scenario.isFailed()) {
+                    byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+                    scenario.attach(screenshot, "image/png", scenario.getName());
+                }
+            } else {
+                byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+                scenario.attach(screenshot, "image/png", scenario.getName());
+            }
+        }
+    }
+}
